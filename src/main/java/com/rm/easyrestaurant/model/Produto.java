a531f3cd721fcbef63ff27219ccf57d3a2702041
DESKTOP-9MT3J76\Rafael Monteiro <rafajapones@gmail.com>
2017-04-09 21:26:22 -0300

    Mapeamento entidade produto.

--- src/main/java/com/rm/easyrestaurant/model/Produto.java
+++ src/main/java/com/rm/easyrestaurant/model/Produto.java
@@ -2,20 +2,59 @@ package com.rm.easyrestaurant.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
 import org.hibernate.validator.constraints.NotBlank;
 
+@Entity
+@Table(name = "produto")
 public class Produto {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+
 	@NotBlank(message = "SKU é obrigatório")
+	@Column(length = 8, nullable = false, unique = true)
 	private String sku;
-	
+
 	@NotBlank(message = "Nome é obrigatório")
+	@Column(length = 100, nullable = false)
 	private String nome;
-	
+
 	private String descricao;
+
+	@NotNull(message = "Valor unitário é obrigatório")
+	@Column(name = "valor_unitario", nullable = false, precision = 10, scale = 2)
+	private BigDecimal valorUnitario;
+
+	@NotNull(message = "Quantidade estoque é obrigatório")
+	@Column(name = "quantidade_estoque", nullable = false)
+	private Integer quantidadeEstoque;
+
+	@NotNull(message = "Categoria é obrigatório")
+	@ManyToOne
+	@JoinColumn(name = "codigo_categoria", nullable = false)
+	private Categoria categoria;
 	
-	private BigDecimal valor;
-	
+	private boolean ativo;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
 	public String getSku() {
 		return sku;
 	}
@@ -39,13 +78,62 @@ public class Produto {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	
-	public BigDecimal getValor() {
-		return valor;
+
+	public BigDecimal getValorUnitario() {
+		return valorUnitario;
+	}
+
+	public void setValorUnitario(BigDecimal valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+
+	public Integer getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+
+	public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
 	}
 	
-	public void setValor(BigDecimal valor) {
-		this.valor = valor;
+	public boolean isAtivo() {
+		return ativo;
 	}
 	
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+
 }
