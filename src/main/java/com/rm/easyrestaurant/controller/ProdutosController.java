a0fed99e23f1a939c863ac5ede6abc9f2778c476
DESKTOP-9MT3J76\Rafael Monteiro <rafajapones@gmail.com>
2017-04-10 21:14:50 -0300

    Montando combo de categoria no cadastro de produto.

--- src/main/java/com/rm/easyrestaurant/controller/ProdutosController.java
+++ src/main/java/com/rm/easyrestaurant/controller/ProdutosController.java
@@ -2,33 +2,42 @@ package com.rm.easyrestaurant.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.rm.easyrestaurant.model.Produto;
+import com.rm.easyrestaurant.repository.Categorias;
 
 @Controller
 @RequestMapping("/produtos")
 public class ProdutosController {
+	
+	@Autowired
+	private Categorias categorias;
 
 	@RequestMapping("/novo")
-	public String novo(Produto produto) {
-		return "produtos/CadastroProduto";
+	public ModelAndView novo(Produto produto) {
+		ModelAndView mv = new ModelAndView("produtos/CadastroProduto");
+		mv.addObject("categorias", categorias.findAll());
+		
+		return mv;
 	}
 	
 	@RequestMapping(value = "/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Produto produto, BindingResult result, 
+	public ModelAndView cadastrar(@Valid Produto produto, BindingResult result, 
 			Model model, RedirectAttributes attributes) {
 		
 		if(result.hasErrors())
 			return novo(produto);
 		
 		attributes.addFlashAttribute("mensagem", "Cadastro realizado com sucesso.");
-		return "redirect:/produtos/novo";
+		return new ModelAndView("redirect:/produtos/novo");
 	}
 	
 }
--- src/main/java/com/rm/easyrestaurant/repository/Categorias.java
+++ src/main/java/com/rm/easyrestaurant/repository/Categorias.java
@@ -3,10 +3,10 @@ package com.rm.easyrestaurant.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.rm.easyrestaurant.model.Produto;
+import com.rm.easyrestaurant.model.Categoria;
 
 @Repository
-public interface Produtos extends JpaRepository<Produto, Long> {
+public interface Categorias extends JpaRepository<Categoria, Long> {
 
-	
+		
 }
