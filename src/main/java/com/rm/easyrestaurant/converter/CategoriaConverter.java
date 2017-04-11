2ee5d148a8d0dfb89e721e997668c0cab18218de
DESKTOP-9MT3J76\Rafael Monteiro <rafajapones@gmail.com>
2017-04-11 19:58:15 -0300

    Exibindo mensagens no cadastro de produto.

--- src/main/java/com/rm/easyrestaurant/converter/CategoriaConverter.java
+++ src/main/java/com/rm/easyrestaurant/converter/CategoriaConverter.java
@@ -1,6 +1,7 @@
 package com.rm.easyrestaurant.converter;
 
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.util.StringUtils;
 
 import com.rm.easyrestaurant.model.Categoria;
 
@@ -8,6 +9,9 @@ public class CategoriaConverter implements Converter<String, Categoria> {
 
 	@Override
 	public Categoria convert(String codigo) {
+		if(StringUtils.isEmpty(codigo))
+			return null;
+			
 		return new Categoria(Long.valueOf(codigo));
 	}
 
