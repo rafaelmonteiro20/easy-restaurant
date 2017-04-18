63ec62e583cd782545361941a64207fd5953d628
DESKTOP-9MT3J76\Rafael Monteiro <rafajapones@gmail.com>
2017-04-17 22:31:27 -0300

    Listando produtos

--- src/main/java/com/rm/easyrestaurant/controller/ProdutosController.java
+++ src/main/java/com/rm/easyrestaurant/controller/ProdutosController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.rm.easyrestaurant.model.Produto;
 import com.rm.easyrestaurant.repository.Categorias;
+import com.rm.easyrestaurant.repository.Produtos;
 import com.rm.easyrestaurant.service.CadastroProdutoService;
 
 @Controller
@@ -25,11 +27,13 @@ public class ProdutosController {
 	@Autowired
 	private CadastroProdutoService service;
 	
+	@Autowired
+	private Produtos produtos;
+	
 	@RequestMapping("/novo")
 	public ModelAndView novo(Produto produto) {
 		ModelAndView mv = new ModelAndView("produtos/CadastroProduto");
 		mv.addObject("categorias", categorias.findAll());
-		
 		return mv;
 	}
 	
@@ -44,6 +48,14 @@ public class ProdutosController {
 		
 		attributes.addFlashAttribute("mensagem", "Cadastro realizado com sucesso.");
 		return new ModelAndView("redirect:/produtos/novo");
+	}
+	
+	@GetMapping
+	public ModelAndView pesquisar() {
+		ModelAndView mv = new ModelAndView("produtos/PesquisaProdutos");
+		mv.addObject("categorias", categorias.findAll());
+		mv.addObject("produtos", produtos.findAll());
+		return mv;
 	}
 	
 }
