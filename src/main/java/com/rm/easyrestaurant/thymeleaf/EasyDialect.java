81829b8435ac8743abb0e4d8d5ee8a3ee448fba3
DESKTOP-9MT3J76\Rafael Monteiro <rafajapones@gmail.com>
2017-05-02 22:28:05 -0300

    Dialect e Processadores thymeleaf

--- src/main/java/com/rm/easyrestaurant/thymeleaf/EasyDialect.java
+++ src/main/java/com/rm/easyrestaurant/thymeleaf/EasyDialect.java
@@ -7,6 +7,8 @@ import org.thymeleaf.dialect.AbstractProcessorDialect;
 import org.thymeleaf.processor.IProcessor;
 import org.thymeleaf.standard.StandardDialect;
 
+import com.rm.easyrestaurant.thymeleaf.processor.ClassForErrorAttributeTagProcessor;
+import com.rm.easyrestaurant.thymeleaf.processor.MessageElementTagProcessor;
 import com.rm.easyrestaurant.thymeleaf.processor.OrderElementTagProcessor;
 import com.rm.easyrestaurant.thymeleaf.processor.PaginationElementTagProcessor;
 
@@ -19,6 +21,8 @@ public class EasyDialect extends AbstractProcessorDialect {
 	@Override
 	public Set<IProcessor> getProcessors(String dialectPrefix) {
 		final Set<IProcessor> processadores = new HashSet<>();
+		processadores.add(new ClassForErrorAttributeTagProcessor(dialectPrefix));
+		processadores.add(new MessageElementTagProcessor(dialectPrefix));
 		processadores.add(new OrderElementTagProcessor(dialectPrefix));
 		processadores.add(new PaginationElementTagProcessor(dialectPrefix));
 		return processadores;
