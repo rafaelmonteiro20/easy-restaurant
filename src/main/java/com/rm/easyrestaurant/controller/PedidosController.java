c61851f321c576847508f783473e47b516012994
Rafael Monteiro <rafajapones@gmail.com>
2017-08-21 21:16:11 -0300

    Prototipando tela de pedido

---
+++ src/main/java/com/rm/easyrestaurant/controller/PedidosController.java
@@ -0,0 +1,17 @@
+package com.rm.easyrestaurant.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/pedidos")
+public class PedidosController {
+
+	@GetMapping("/form")
+	public ModelAndView form() {
+		return new ModelAndView("pedidos/CadastroPedido");
+	}
+	
+}
