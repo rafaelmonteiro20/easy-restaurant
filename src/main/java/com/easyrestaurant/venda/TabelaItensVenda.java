1a76ddb25808c5b6fb17ec263949de31da6eec1e
Rafael Monteiro <rafajapones@gmail.com>
2017-11-03 22:46:21 -0300

    Adicionando itens na tabela itens de venda

--- src/main/java/com/easyrestaurant/venda/TabelaItensVenda.java
+++ src/main/java/com/easyrestaurant/venda/TabelaItensVenda.java
@@ -5,10 +5,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.easyrestaurant.model.ItemVenda;
+import com.easyrestaurant.model.Produto;
 
 public class TabelaItensVenda {
 
 	private List<ItemVenda> itens = new ArrayList<>();
+	
+	public void adicionarItem(Produto produto, Integer quantidade) {
+		ItemVenda item = new ItemVenda();
+		item.setProduto(produto);
+		item.setQuantidade(quantidade);
+		item.setValorUnitario(produto.getValorUnitario());
+		
+		itens.add(item);
+	}
 	
 	public BigDecimal getValorTotal() {
 		return itens.stream()
--- src/test/java/com/easyrestaurant/venda/TabelaItensVendaTest.java
+++ src/test/java/com/easyrestaurant/venda/TabelaItensVendaTest.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.easyrestaurant.venda.TabelaItensVenda;
+import com.easyrestaurant.model.Produto;
 
 public class TabelaItensVendaTest {
 
@@ -19,8 +19,35 @@ public class TabelaItensVendaTest {
 	}
 	
 	@Test
-	public void deveCalcularValorTotalSemItens() throws Exception {
+	public void deveCalcularValorTotalSemItens() {
 		assertEquals(BigDecimal.ZERO, tabelaItensVenda.getValorTotal());
+	}
+	
+	@Test
+	public void deveCalcularValorTotalComUmItem() {
+		Produto produto = new Produto();
+		BigDecimal valor = new BigDecimal("8.90");
+		produto.setValorUnitario(valor);
+		
+		tabelaItensVenda.adicionarItem(produto, 1);
+		
+		assertEquals(valor, tabelaItensVenda.getValorTotal());
+	}
+	
+	@Test
+	public void deveCalcularValorTotalComVariosItens() {
+		Produto p1 = new Produto();
+		BigDecimal v1 = new BigDecimal("8.90");
+		p1.setValorUnitario(v1);
+		
+		Produto p2 = new Produto();
+		BigDecimal v2 = new BigDecimal("4.50");
+		p2.setValorUnitario(v2);
+		
+		tabelaItensVenda.adicionarItem(p1, 1);
+		tabelaItensVenda.adicionarItem(p2, 2);
+		
+		assertEquals(new BigDecimal("17.90"), tabelaItensVenda.getValorTotal());
 	}
 	
 }
