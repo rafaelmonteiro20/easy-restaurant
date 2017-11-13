fd52c1b4cf96167a6355e8328cc9e9d708e3e732
Rafael Monteiro <rafajapones@gmail.com>
2017-11-12 21:22:23 -0300

    Atualizando a tabela de itens após adicionar um item existente.

--- src/main/resources/static/js/pedido.autocomplete-itens.js
+++ src/main/resources/static/js/pedido.autocomplete-itens.js
@@ -13,8 +13,8 @@ Easy.Autocomplete = (function() {
 	Autocomplete.prototype.iniciar = function() {
 		var options = {
 			url: function(skuOuNome) {
-				return '/easy-restaurant/produtos?skuOuNome=' + skuOuNome;
-			},
+				return this.skuOuNomeInput.data('url') + '?skuOuNome=' + skuOuNome;
+			}.bind(this),
 			getValue: 'nome',
 			minCharNumber: 3,
 			requestDelay: 300,
@@ -35,6 +35,8 @@ Easy.Autocomplete = (function() {
 		
 	function onItemSelecionado() {
 		this.emitter.trigger('item-selecionado', this.skuOuNomeInput.getSelectedItemData());
+		this.skuOuNomeInput.val('');
+		this.skuOuNomeInput.focus();
 	}
 		
 	function template(nome, produto) {
