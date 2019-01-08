c90967bdf6522bc4995a0c269ffa6433bcc3b558
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2019-01-07 22:27:24 -0300

    Configurando Spring Security

--- src/main/java/com/easyrestaurant/config/SecurityConfig.java
+++ src/main/java/com/easyrestaurant/config/SecurityConfig.java
@@ -1,8 +1,37 @@
 package com.easyrestaurant.config;
 
-//@Configuration
-public class SecurityConfig {
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+//				.loginPage("/login")
+//				.permitAll()
+				.and()
+			.csrf().disable();
+	}
+	
+	@Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+          		.withUser("admin")
+          		.password(encoder.encode("123"))
+          		.roles("USER");
+    }
+	
 //	@Bean
 //	public PasswordEncoder passwordEncoder() {
 //		return new BCryptPasswordEncoder();
